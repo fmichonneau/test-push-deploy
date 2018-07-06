d8d7f565c752f1c294ba15add5eaf3be5183be64
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-07-06 14:01:26 +0000

    Deploy fmichonneau/test-push-deploy to github.com/fmichonneau/test-push-deploy.git:gh-pages

---
+++ amy_add.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+REPO_ORG=fmichonneau
+REPO_NAME=test-push-deploy
+
+mkdir -p _data
+
+cd .. &&
+    rm -rf amy-feeds &&
+    git clone https://github.com/carpentries/amy-feeds.git amy-feeds &&
+    cd amy-feeds &&
+    make amy &&
+    cp _data/dc_amy.yml ../"$REPO_NAME"/_data/amy.yml
+
+cd ../"$REPO_NAME" || exit
+#git remote add deploy git@github.com:"$REPO_ORG"/"$REPO_NAME".git
+
+git checkout master
+git add _data/amy.yml
+git commit -m "[ci skip] update workshop data"
+#git push deploy master
